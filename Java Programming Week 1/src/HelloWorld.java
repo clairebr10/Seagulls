2737d1f2a6d0ee0fc80ab2ecce3f6666c107c116
Claire Bradford <claire@wonderland.org>
2016-08-29 17:11:23 +1000

    New

---
+++ Java Programming Week 1/src/HelloWorld.java
@@ -0,0 +1,11 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		int number=10;
+		System.out.println("Hello World");
+		System.out.println("My number is : " + number);
+
+	}
+
+}
